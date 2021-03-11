7302da12d4dafddedd35d604cf9d711fe67f70ce
Barry Molina <bazzaboy@gmail.com>
2021-03-11 02:29:31 -0600

    delete commented code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import styled from "styled-components"
 
 import Layout from "../components/layout"
@@ -25,16 +25,11 @@ const Welcome = styled.div`
 `
 
 const ImageSlider = styled(GatsbyImage)`
-  /* animation: fadeIn ease 2s;
-  @keyframes fadeIn {
-    from { opacity: 0; }
-    to { opacity: 1; }
-  } */
-  /* transition: opacity 1s;
-  opacity: ${({ state }) => (state === "entering" || state === "entered") ? 1 : 0}; */
   position: absolute;
   top: 5%;
   left: 22%;
+
+  /* classes added and removed by TransitionGroup */
   &.image-enter {
     opacity: 0;
   }
@@ -49,57 +44,35 @@ const ImageSlider = styled(GatsbyImage)`
     opacity: 0;
     transition: opacity ${transitionSpeed}s;
   }
-  /* &.image-leave-done {
-    opacity: 0
-  } */
 `
 
 const IndexPage = ({ data }) => { 
   const [index, setIndex] = useState(0)
-  const [playSlides, setPlaySlides] = useState(true)
-  // const [animate, setAnimate] = useState(false)
 
   const slides = data.allMarkdownRemark.nodes
   const lenSlides = slides.length - 1;
-  console.log(slides)
 
   // executes when component renders or re-renders
   useEffect( () => {
     let timer
 
-    if (playSlides) {
-      timer = setTimeout( () => {
-        // pass in a function that receives the current and then updates it
-        // setIndex(currIndex => currIndex + 1)
-        nextSlide()
-      }, slideSpeed * 1000)
-    }
+    timer = setTimeout( () => {
+      nextSlide()
+    }, slideSpeed * 1000)
+
+    // clean up function is called when component unmounts
     return () => {
       clearTimeout(timer)
     }
   })
 
   const nextSlide = () => {
-    // setAnimate(false)
-    // setAnimate(false)
-    // setTimeout(() => { 
-    //   setIndex(prevIdx => prevIdx === lenSlides ? 0 : prevIdx + 1)
-    //   setAnimate(true)
-    // }, 1000)
-    // setAnimate(true)
       setIndex(prevIdx => prevIdx === lenSlides ? 0 : prevIdx + 1)
   }
 
-  const previousSlide = () => {
-    // setAnimate(false)
-    // setTimeout(() => { 
-    //   setIndex(prevIdx => prevIdx === 0 ? lenSlides : prevIdx - 1)
-    //   setAnimate(true)
-    // }, 1000)
-    // setIndex(prevIdx => prevIdx === 0 ? lenSlides : prevIdx - 1)
-    // setAnimate(true)
-      setIndex(prevIdx => prevIdx === 0 ? lenSlides : prevIdx - 1)
-  }
+  // const previousSlide = () => {
+  //     setIndex(prevIdx => prevIdx === 0 ? lenSlides : prevIdx - 1)
+  // }
 
   return (
     <Layout>
@@ -107,27 +80,15 @@ const IndexPage = ({ data }) => {
       <Welcome>
         <p>My name is Emily.</p>
         <p>I paint stuff.</p>
-
-        {/* <Transition in={animate} timeout={1000}>
-          {(state) => (
-            <ImageSlider state={state} image={getImage(slides[index].frontmatter.image)}/>
-          )}
-        </Transition> */}
-        {/* <CSSTransition in={animate} timeout={2000} classNames="image">
-          <ImageSlider image={getImage(slides[index].frontmatter.image)}/>
-        </CSSTransition> */}
-        {/* <p>We're on index {index}</p> */}
-        {/* <button onClick={() => setIndex(0)}>reset</button>
-        <button onClick={() => previousSlide()}>-</button>
-        <button onClick={() => nextSlide()}>+</button>
-        <button onClick={() => setPlaySlides(play => !play)}>toggle timer</button>
-        <button onClick={() => setAnimate(animate => !animate)}>Toggle animate</button> */}
       </Welcome>
-        <TransitionGroup component={null}>
-          <CSSTransition key={index} timeout={transitionSpeed * 1000} classNames="image">
-            <ImageSlider image={getImage(slides[index].frontmatter.image)}/>
-          </CSSTransition>
-        </TransitionGroup>
+      TransitionGroup will 
+      <TransitionGroup component={null}>
+        {/* This Transition component will be swapped out by TransitionGroup when the key attribute changes. */}
+        {/* Class names starting with image-* will be applied while entering and leaving */}
+        <CSSTransition key={index} timeout={transitionSpeed * 1000} classNames="image">
+          <ImageSlider image={getImage(slides[index].frontmatter.image)}/>
+        </CSSTransition>
+      </TransitionGroup>
     </Layout>
   )
  }
